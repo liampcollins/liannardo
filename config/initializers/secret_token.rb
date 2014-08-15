8e7030b11e8300b5507a88590fef244b0ff997ab
Liam <liampcollins@gmail.com>
2014-08-15 16:47:56 +0100

    adding rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liannardo::Application.config.secret_token = '61903565125dcaa7af2d6140bfa807ba303e5d91ac39818a2c3692f4b241d7bda23df0be6dedb20469a57bdb72fc823cfd1b9a201515d8b5b07b99d10c1459a0'
