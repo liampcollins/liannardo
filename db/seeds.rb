e88e326bcef26761f75a15e2b02df5b68e146435
Johanna <johanna.carlberg@bloglovin.com>
2014-08-16 12:48:54 +0100

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Post.delete_all
+# Vote.delete_all
+
+u1 = User.create!(name: 'Liam', email: 'liamcollins@gmail.com', password:'password', password_confirmation: 'password', image:'https://media.licdn.com/mpr/mpr/wc_200_200/p/4/005/025/0a4/16ff8cb.jpg' , bio:'I am Irish')
+
+p1 = Post.create!(content: 'I like boats')
+p2 = Post.create!(content: 'I hate cats')
+
+u1.posts << [p1]
+u1.posts << [p2]
