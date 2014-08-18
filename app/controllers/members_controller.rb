d964db54d1666e384cc4c2ec0a4a4f5682210dce
Johanna <johanna.carlberg@bloglovin.com>
2014-08-18 17:40:48 +0100

    got members controller sorted out

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,13 +1,47 @@
 class MembersController < ApplicationController
   def index
+    @q = Member.search(params[:q])
+    if params[:q]
+        # raise
+    end
+    @users = @q.result(distinct: true)
+    # @users = Member.all
+    # @p = User.search(params[:p])
+    # @users = @p.result(distinct: true)
+
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
+
   end
 
   def update
+    @user = User.find(params[:id])
+
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
