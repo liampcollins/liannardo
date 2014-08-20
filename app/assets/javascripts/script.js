4bfa10dd22901eb5192db69d38ed6fcefbba37c0
Liam <liampcollins@gmail.com>
2014-08-20 16:39:18 +0100

    updated image sizes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -140,16 +140,39 @@ function toggleFollow(){
 
 
 
+function updateReposts(button, post){
+  console.log("button")
+  if($(button).hasClass("reposted")){
+    post.children('.reposts').html(parseInt(post.children('.reposts').html()) +1)
+  }else{
+    post.children('.reposts').html(parseInt(post.children('.reposts').html()) -1)
+  }
+}
 
-// function rePost(){
-//   console.log("respost")
-//   $this = $(this)
-//   $post = $this.parent().parent()
+function rePost(){
+  console.log("respost")
+  $this = $(this)
+  $post = $this.parent().parent()
+  id = parseInt($post.children('.id').html())
+  request("POST", "/posts", {post:{repost_id: id}}).success(console.log("success")).success(function(){
+    $this.toggleClass("not_reposted").toggleClass("reposted")
+    }).success(function(){
+      updateReposts($this, $post)
+  })
+}
 
-// get post details (publisher, content, likes, hates)
-// append to the top of the list of your tweets on your profile page
-//If your followers don't follow publisher post to their timelines too
-// }
+
+function postForm(e){
+  e.preventDefault()
+  console.log("postForm")
+  $this = $(this)
+  $post = $this.parent()
+  content = $post.children('input').val()
+  request("POST", "/posts", {post:{content: content}}).success(console.log("success")).success(function(){
+    appendNewPost($post)
+  })
+
+}
   
 $(function(){
   $('.like').on('click', toggleLike);
@@ -157,6 +180,8 @@ $(function(){
   //$(".repost").on('click', rePost);
   $(".not_following").on('click', toggleFollow);
   $(".following").on('click', toggleFollow);
+  $('#myModal .btn-primary').on("click", postForm);
+  $(".repost").on('click', rePost);
   //$('button').tooltip();
 })
 
